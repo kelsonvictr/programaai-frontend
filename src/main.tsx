f7aa53163da101caa406ed52a6c37e7abf934795
kelsonalmeida <kelson.victr@gmail.com>
2025-06-21 11:11:36 -0300

    fix: index.css

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import "./styles/custom.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import './index.css'
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
