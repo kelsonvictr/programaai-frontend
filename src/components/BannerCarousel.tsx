d87cc4026c30ed59153653f0875b96e92a5d4b7f
kelsonalmeida <kelson.victr@gmail.com>
2025-07-14 12:33:39 -0300

    feat: new banners

--- src/components/BannerCarousel.tsx
+++ src/components/BannerCarousel.tsx
@@ -1,6 +1,7 @@
 // src/components/BannerCarousel.tsx
 import React from "react"
 import { Carousel } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import styles from "./BannerCarousel.module.css"
 import { courses } from "../mocks/courses"
 
@@ -8,11 +9,13 @@ const BannerCarousel: React.FC = () => (
   <Carousel controls indicators interval={5000} className={styles.carousel}>
     {courses.map(c => (
       <Carousel.Item key={c.id} className={styles.item}>
-        <img
-          className={styles.image}
-          src={`${c.bannerSite}`}
-          alt={c.title}
-        />
+        <Link to={`/cursos/${c.id}`}>
+          <img
+            className={styles.image}
+            src={`${c.bannerSite}`}
+            alt={c.title}
+          />
+        </Link>
       </Carousel.Item>
     ))}
   </Carousel>
